2865c7859f10659bb20770c39f43e44ac3b84d29
Spencer K-W <keashlywarters@gmail.com>
2019-05-30 15:35:27 -0400

    initial project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/profile", pathMatch: "full"},
+  {path: "profile", component: ProfileComponent},
+  {path: "edit-profile", component: EditComponent},
+  {path: "**", redirectTo: "/profile"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
