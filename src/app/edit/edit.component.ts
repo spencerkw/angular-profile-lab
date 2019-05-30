8705a3abea042575b473ba6bda632bdce41e86cb
Spencer K-W <keashlywarters@gmail.com>
2019-05-30 16:29:48 -0400

    edit component functionality done

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserProfile } from '../user-profile';
+import { ProfileService } from '../profile.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'edit',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  profile: UserProfile;
+
+  constructor(private profileService: ProfileService, private router: Router) { }
 
   ngOnInit() {
+    this.profile = this.profileService.getUserProfile();
+  }
+
+  submitProfile(form) {
+    console.log(form);
+    let newProfile: UserProfile = {
+      name: form.value.name,
+      contactInfo: form.value.contactInfo,
+      bio: form.value.bio
+    };
+    this.profileService.setUserProfile(newProfile);
+
+    this.router.navigate(["profile"]);
   }
 
 }
